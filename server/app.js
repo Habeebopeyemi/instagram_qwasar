88eb46411b3be4465dcde1a44dffc963545a8d71
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2023-07-11 13:17:17 +0100

    ...

--- server/app.js
+++ server/app.js
@@ -3,8 +3,22 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+const customMiddleware = (req, res, next) => {
+  console.log("middleware executed!!");
+  next();
+};
+/*middle run for every request */
+// app.use(customMiddleware);
+
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+/*middle run for only /about request */
+
+app.get("/about", customMiddleware, (req, res) => {
+  console.log("about");
+  res.send("about page");
 });
 
 app.listen(PORT, () => {
