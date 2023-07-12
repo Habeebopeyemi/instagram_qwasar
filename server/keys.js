0f13e40cb02de7976a9edeed05181225e702355f
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2023-07-12 10:26:16 +0100

    sign in almost completed

--- server/keys.js
+++ server/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
   MONGODBURI: process.env.INSTAGRAM_QWASAR_DB,
+  JWT_SECRET: process.env.INSTAGRAM_JWT_SECRET,
 };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
 
 const router = express.Router();
+const User = mongoose.model("User");
 
 router.get("/", (req, res, next) => {
   res.send("hello");
@@ -11,7 +15,53 @@ router.post("/signup", (req, res, next) => {
   if (!email || !password || !name) {
     res.status(422).json({ error: "please add all the fields" });
   }
-    res.json({message:"successfully posted"})
+  User.findOne({ email: email })
+    .then(user => {
+      if (user)
+        return res
+          .status(422)
+          .json({ error: "user already exist with that email" });
+      bcrypt.hash(password, 12).then(hashed_password => {
+        const new_user = new User({
+          email: email,
+          password: hashed_password,
+          name: name,
+        });
+        new_user
+          .save()
+          .then(user => {
+            res.status(200).json({ message: "signup successfully" });
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+router.post("/signin", (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    return res
+      .status(422)
+      .json({ error: "please add valid email or password" });
+
+  User.findOne({ email: email }).then(user => {
+    if (!user) res.status(422).json({ error: "invalid email or password" });
+    bcrypt
+      .compare(password, user.password)
+      .then(onFulfilled => {
+        if (onFulfilled) {
+          return res.json({ message: "sign in successful" });
+        } else {
+          return res.status(422).json({ error: "invalid email or password" });
+        }
+      })
+      .catch(err => console.log(err));
+  });
 });
 
 module.exports = router;
